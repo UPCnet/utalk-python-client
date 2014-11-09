23cd9b3c755fe6538fde79691a1e2e63e07e9d02
Carles Bruguera <carlesba@gmail.com>
2014-11-09 12:50:26 +0100

    Redefine testclient

--- utalkpythonclient/testclient.py
+++ utalkpythonclient/testclient.py
@@ -8,16 +8,17 @@ import gevent
 
 class UTalkTestClient(UTalkClient):
 
-    def setup(self, send, expect, ready):
+    def setup(self, conversation, send, expect, ready):
         """
             Configures the test
         """
         self.to_send = send
-        self.to_expect = expect
         self.wait_send = ready
+        self.conversation = conversation
 
-        self.expected_messages = len(self.to_expect) + len(self.to_send)
-        self.expected_acks = self.expected_messages
+        self.expected_messages = send + expect
+        self.expected_acks = send + expect
+
         self.received_messages = 0
         self.ackd_messages = 0
 
@@ -40,9 +41,17 @@ class UTalkTestClient(UTalkClient):
         self.test_finished()
 
     def test_finished(self):
-        #print '{} AKCD:{}, RECV:{}'.format(self.username, self.ackd_messages, self.received_messages)
+        # print '{} AKCD:{}, RECV:{}'.format(self.username, self.ackd_messages, self.received_messages)
         if self.succeded():
             self.disconnect()
+
+    @property
+    def messages(self):
+        """
+            Generates numbered messages
+        """
+        for i in range(self.to_send):
+            yield (self.conversation, 'This is Message {} from {}'.format(i, self.username))
 
     def on_start_listening(self):
         self.wait_send.ready()
@@ -50,7 +59,7 @@ class UTalkTestClient(UTalkClient):
         self.wait_send.event.get()
 
         self.log("start sending {} messages".format(self.username))
-        for conversation_id, text in self.to_send:
+        for conversation_id, text in self.messages:
             gevent.sleep()
             self.send_message(conversation_id, text)
 
