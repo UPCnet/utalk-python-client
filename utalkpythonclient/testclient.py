d0007eb869e808022345656512757ff3ea68f96d
Carles Bruguera <carlesba@gmail.com>
2014-11-12 10:17:47 +0100

    Don't block when waiting to send a message Save time stats with sent date Don't start immediately, wait start_delay

--- utalkpythonclient/testclient.py
+++ utalkpythonclient/testclient.py
@@ -1,18 +1,20 @@
 from utalkpythonclient.client import UTalkClient
 from gevent.monkey import patch_all
 import gevent
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class UTalkTestClient(UTalkClient):
 
-    def setup(self, conversation, send, expect, ready):
+    def setup(self, conversation, send, expect, ready, start_delay=0, message_delay=0):
         """
             Configures the test
         """
         self.to_send = send
         self.wait_send = ready
         self.conversation = conversation
+        self.start_delay = start_delay
+        self.message_delay = message_delay
 
         self.expected_messages = send + expect
         self.expected_acks = send + expect
@@ -21,22 +23,25 @@ class UTalkTestClient(UTalkClient):
         self.ackd_messages = 0
         self.stats = {
             "recv_times": [],
-            "ackd_times": []
+            "ackd_times": [],
+            "send_times": []
         }
 
     def succeded(self):
-        return self.received_messages == self.expected_messages and \
-            self.ackd_messages == self.expected_acks
+        return self.received_messages >= self.expected_messages and \
+            self.ackd_messages >= self.expected_acks
 
-    def on_connecting(self):
-        patch_all()
+    # def on_connecting(self):
+    #     patch_all()
 
     def on_message_received(self, message):
         # Collect stats for messages received from other users
         if self.username != message.json['u']['u']:
             now = datetime.utcnow()
-            message_elapsed = now - datetime.strptime(message.json['p'], '%Y-%m-%dT%H:%M:%SZ')
-            self.stats['recv_times'].append(message_elapsed.total_seconds())
+            sent = datetime.strptime(message.json['p'], '%Y-%m-%dT%H:%M:%S.%fZ')
+            self.stats['recv_times'].append((sent, now))
+        else:
+            self.stats['send_times'].append(datetime.strptime(message.json['p'], '%Y-%m-%dT%H:%M:%S.%fZ'))
 
         self.received_messages += 1
         self.test_finished()
@@ -47,8 +52,8 @@ class UTalkTestClient(UTalkClient):
     def on_message_ackd(self, message):
         if self.username != message.json['u']['u']:
             now = datetime.utcnow()
-            message_elapsed = now - datetime.strptime(message.json['p'], '%Y-%m-%dT%H:%M:%SZ')
-            self.stats['ackd_times'].append(message_elapsed.total_seconds())
+            sent = datetime.strptime(message.json['p'], '%Y-%m-%dT%H:%M:%S.%fZ')
+            self.stats['ackd_times'].append((sent, now))
 
         self.ackd_messages += 1
         self.test_finished()
@@ -68,13 +73,17 @@ class UTalkTestClient(UTalkClient):
 
     def on_start_listening(self):
         self.wait_send.ready()
-        gevent.sleep(1)
+        gevent.sleep()
         self.wait_send.event.get()
+        gevent.sleep(self.start_delay)
 
         self.log("start sending {} messages".format(self.username))
+        next_message_date = datetime.utcnow()
         for conversation_id, text in self.messages:
-            gevent.sleep()
+            while datetime.utcnow() <= next_message_date:
+                gevent.sleep()
             self.send_message(conversation_id, text)
+            next_message_date = datetime.utcnow() + timedelta(seconds=self.message_delay)
 
     def teardown(self):
         """
